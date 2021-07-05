0e5b1337e4896ea40a326e8c094f0b3218004b18
TaufikAkbar7 <taufik.lol911@gmail.com>
2021-07-05 21:14:43 +0700

    add meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import { Fragment } from "react"
 import { Course, Hero, Footer, Navbar, TopFeatured, About, Testimoni } from "../components"
+import Head from "next/head"
+
 export default function Home() {
   return (
     <Fragment>
-      <Navbar/>
-      <Hero/>
-      <TopFeatured/>
-      <Course/>
-      <About/>
-      <Testimoni/>
-      <Footer/>
-    </Fragment>    
+      <Head>
+        <title>Landing Page Coding Nicely</title>
+        <meta name="description" content="Company Profile website using Next JS" />
+      </Head>
+      <Navbar />
+      <Hero />
+      <TopFeatured />
+      <Course />
+      <About />
+      <Testimoni />
+      <Footer />
+    </Fragment>
   )
 }
